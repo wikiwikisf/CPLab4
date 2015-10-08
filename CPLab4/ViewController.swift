b950e0234251222b0d3e5c81228b2b3fd6b45404
Christopher McMahon <melophonic@gmail.com>
2015-10-07 18:59:14 -0700

    Initial Commit

---
+++ CPLab4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CPLab4
+//
+//  Created by Christopher McMahon on 10/7/15.
+//  Copyright © 2015 codepath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
